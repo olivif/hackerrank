fd8e19884beae1d641de977ffc3066fbe830f064
Olivia Ifrim <olly2204@yahoo.com>
2016-08-09 22:54:41 +0200

    Implementing TripletsScore.

---
+++ HackerRank.Tests/Algorithms/Warmup/TripletsTests.cs
@@ -0,0 +1,65 @@
+﻿namespace HackerRank.Algorithms.Warmup
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TripletsTests
+    {
+        [TestMethod]
+        public void TripletScore_Compare_LessThan()
+        {
+            // Arrange
+            var triplet1 = new TripletScore(1, 2, 3);
+            var triplet2 = new TripletScore(4, 2, 3);
+
+            // Act
+            var comparisonValue = triplet1.CompareTo(triplet2);
+
+            // Assert
+            Assert.AreEqual(0, comparisonValue);
+        }
+
+        [TestMethod]
+        public void TripletScore_Compare_GreaterThan()
+        {
+            // Arrange
+            var triplet1 = new TripletScore(1, 2, 3);
+            var triplet2 = new TripletScore(0, 2, 3);
+
+            // Act
+            var comparisonValue = triplet1.CompareTo(triplet2);
+
+            // Assert
+            Assert.AreEqual(1, comparisonValue);
+        }
+
+        [TestMethod]
+        public void TripletScore_Compare_Equal()
+        {
+            // Arrange
+            var triplet = new TripletScore(1, 2, 3);
+
+            // Act
+            var comparisonValue = triplet.CompareTo(triplet);
+
+            // Assert
+            Assert.AreEqual(0, comparisonValue);
+        }
+
+        [TestMethod]
+        public void TripletScore_Compare_HackerRank()
+        {
+            // Arrange
+            var triplet1 = new TripletScore(5, 6, 7);
+            var triplet2 = new TripletScore(3, 6, 10);
+
+            // Act
+            var comparisonValue1 = triplet1.CompareTo(triplet2);
+            var comparisonValue2 = triplet2.CompareTo(triplet1);
+
+            // Assert
+            Assert.AreEqual(1, comparisonValue1);
+            Assert.AreEqual(1, comparisonValue2);
+        }
+    }
+}
---
+++ HackerRank/Algorithms/Warmup/TripletScore.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// https://www.hackerrank.com/challenges/compare-the-triplets
+/// </summary>
+namespace HackerRank.Algorithms.Warmup
+{
+    using System;
+
+    public class TripletScore : IComparable
+    {
+        public TripletScore(int clarity, int originality, int difficulty)
+        {
+            this.Clarity = clarity;
+            this.Originality = originality;
+            this.Difficulty = difficulty;
+        }
+
+        public int Clarity { get; set; }
+
+        public int Originality { get; set; }
+
+        public int Difficulty { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            var other = obj as TripletScore;
+            var comparisonValue = 0;
+
+            comparisonValue += (this.Clarity == other.Clarity) ? 0 : ((this.Clarity > other.Clarity) ? 1 : 0);
+            comparisonValue += (this.Originality == other.Originality) ? 0 : ((this.Originality > other.Originality) ? 1 : 0);
+            comparisonValue += (this.Difficulty == other.Difficulty) ? 0 : ((this.Difficulty > other.Difficulty) ? 1 : 0);
+
+            return comparisonValue;
+        }
+    }
+}
