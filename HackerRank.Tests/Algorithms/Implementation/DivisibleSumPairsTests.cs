9dd729d8f2384310b360c330350306c7ff91e508
Olivia Ifrim <olly2204@yahoo.com>
2016-08-10 00:28:56 +0200

    Implementing DivisibleSumPairs.

---
+++ HackerRank.Tests/Algorithms/Implementation/DivisibleSumPairsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace HackerRank.Algorithms.Implementation
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+
+    [TestClass]
+    public class DivisibleSumPairsTests
+    {
+        [TestMethod]
+        public void DivisibleSumPairs_HackerRank()
+        {
+            // Arrange
+            //6 3
+            //1 3 2 6 1 2
+            var list = new List<int>() { 1, 3, 2, 6, 1, 2 };
+            var k = 3;
+            var divisibleSumPairs = new Solution();
+
+            // Act
+            var count = divisibleSumPairs.GetPairs(list, k);
+
+            // Assert
+            Assert.AreEqual(5, count);
+        }
+    }
+}
---
+++ HackerRank/Algorithms/Implementation/DivisibleSumPairs.cs
@@ -0,0 +1,62 @@
+﻿namespace HackerRank.Algorithms.Implementation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Solution
+    {
+        public void ReadInput(ref IList<int> list, ref int k)
+        {
+            string line;
+            string[] split;
+
+            // n k
+            line = Console.ReadLine();
+            split = line.Split(' ');
+            var numbers = split.ToList().Select(number => int.Parse(number)).ToList();
+            var n = numbers[0];
+            k = numbers[1];
+
+            // numbers
+            line = Console.ReadLine();
+            split = line.Split(' ');
+            list = split.ToList().Select(number => int.Parse(number)).ToList();
+        }
+
+        public int GetPairs(IList<int> list, int k)
+        {
+            var count = 0;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (i < j)
+                    {
+                        if ((list[i] + list[j]) % k == 0)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        static void Main(String[] args)
+        {
+            var divisibleSumPairs = new Solution();
+
+            // Read input
+            IList<int> list = null;
+            int k = 0;
+            divisibleSumPairs.ReadInput(ref list, ref k);
+
+            // Get count
+            var count = divisibleSumPairs.GetPairs(list, k);
+            Console.WriteLine(count);
+        }
+    }
+}
